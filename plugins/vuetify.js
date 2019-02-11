6c97447f0bbd6080f82b4a6b8f2b4d0071c92820
iskanderbroere <iskanderbroere@gmail.com>
2019-02-11 20:09:25 +0100

    conditional theme cache

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import LRU from 'lru-cache'
 import Icon from 'vue-awesome/components/Icon'
+import colors from 'vuetify/es5/util/colors'
 
+const isProd = process.env.NODE_ENV !== 'development'
 const themeCache = new LRU({
   max: 10,
   maxAge: 1000 * 60 * 60 // 1 hour
@@ -10,16 +12,14 @@ const themeCache = new LRU({
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#FF6200',
-    secondary: '#212121'
+    primary: colors.orange.accent4,
+    secondary: colors.grey.darken4
   },
   options: {
-    minifyTheme: function(css) {
-      return process.env.NODE_ENV === 'production'
-        ? css.replace(/[\s|\r\n|\r|\n]/g, '')
-        : css
+    minifyTheme: function (css) {
+      return isProd ? css.replace(/[\s|\r\n|\r|\n]/g, '') : css
     },
-    themeCache
+    themeCache: isProd ? themeCache : null
   },
   icons: {
     next: {
