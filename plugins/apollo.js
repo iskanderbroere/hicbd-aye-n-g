321b103f131b908cbca7f4d8881ed69dacf36bab
iskanderbroere <iskanderbroere@gmail.com>
2019-02-13 19:59:13 +0100

    fix env

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -10,7 +10,6 @@ Vue.use(VueApollo)
 const createApolloClient = ssr => {
   const httpLink = new HttpLink({
     // You should use an absolute URL here
-    // uri: process.env.backendUrl,
     uri: process.env.backendUrl,
     fetch
   })
