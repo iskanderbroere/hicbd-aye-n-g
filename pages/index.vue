77d1ea19fc63ee8a616aac82f24920a633e78096
iskanderbroere <iskanderbroere@gmail.com>
2019-02-28 21:01:17 +0100

    lint

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,66 @@
 <template>
   <main>
     <v-container grid-list-md>
-      <v-layout row wrap>
-        <v-flex tag="h1" xs12 v-if="heading">{{ heading }}</v-flex>
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex
+          v-if="heading"
+          tag="h1"
+          xs12
+        >
+          {{ heading }}
+        </v-flex>
         <v-flex xs12>
-          <lazy-hydrate v-if="brandBars && brandBars.length > 0" when-idle>
-            <i-n-g-carousel :carousel-items="brandBars"/>
+          <lazy-hydrate
+            v-if="brandBars && brandBars.length > 0"
+            when-idle
+          >
+            <i-n-g-carousel :carousel-items="brandBars" />
           </lazy-hydrate>
         </v-flex>
         <v-flex xs12>
-        <v-card class="pa-5 orange accent-4 my-4" raised>
-          <v-card-text align-center>
-            <h3 v-if="callToAction" class="display-3 white--text">{{ callToAction.heading }}</h3>
-            <span v-if="callToAction" class="white--text headline">{{ callToAction.description }}</span>
-            <v-autocomplete
-              v-model="autocompleteSelection"
-              :search-input.sync="searchTerm"
-              label="Type om te zoeken..."
-              solo
-              no-filter
-              append-icon
-              class="my-2"
-              :items="suggestions"
-            />
-          </v-card-text>
-        </v-card>
+          <v-card
+            class="pa-5 orange accent-4 my-4"
+            raised
+          >
+            <v-card-text align-center>
+              <h3
+                v-if="callToAction"
+                class="display-3 white--text"
+              >
+                {{ callToAction.heading }}
+              </h3>
+              <span
+                v-if="callToAction"
+                class="white--text headline"
+              >{{ callToAction.description }}</span>
+              <v-autocomplete
+                v-model="autocompleteSelection"
+                :search-input.sync="searchTerm"
+                label="Type om te zoeken..."
+                solo
+                no-filter
+                append-icon
+                class="my-2"
+                :items="suggestions"
+              />
+            </v-card-text>
+          </v-card>
         </v-flex>
-        <v-flex v-for="i in 3" :key="`4${i}`" xs4>
-          <v-card dark color="primary">
-            <v-card-text class="px-0">4</v-card-text>
+        <v-flex
+          v-for="i in 3"
+          :key="`4${i}`"
+          xs4
+        >
+          <v-card
+            dark
+            color="primary"
+          >
+            <v-card-text class="px-0">
+              4
+            </v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
@@ -80,7 +112,7 @@ export default {
     searchTerm: ''
   }),
   watch: {
-    async searchTerm(newSearchTerm) {
+    async searchTerm (newSearchTerm) {
       try {
         const {
           data: { searchSuggestions }
@@ -99,14 +131,14 @@ export default {
     homePage: {
       query: homePageQuery,
       manual: true,
-      result({ data: { brandBars, heading, callToAction } }) {
+      result ({ data: { brandBars, heading, callToAction } }) {
         this.brandBars = brandBars
         this.heading = heading
         this.callToAction = callToAction
       }
     }
   },
-  head() {
+  head () {
     return {
       title: this.heading,
       meta: [
