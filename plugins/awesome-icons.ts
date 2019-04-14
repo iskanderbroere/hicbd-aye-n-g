aebebaba83b765e45cfbae643516c7fdf364d178
iskanderbroere <iskanderbroere@gmail.com>
2019-04-14 20:36:13 +0200

    fix: build

--- plugins/awesome-icons.ts
+++ plugins/awesome-icons.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Icon from 'vue-awesome/components/Icon'
+import Icon from "vue-awesome/components/Icon.vue"
 import 'vue-awesome/icons/flag'
 import 'vue-awesome/icons/chevron-left'
 import 'vue-awesome/icons/chevron-right'
